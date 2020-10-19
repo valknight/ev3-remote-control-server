b8d938d2eb535e6591a1b899a32d48ff979c34b3
Val Knight <val@valknight.xyz>
2020-10-19 14:06:08 +0100

    Fix formatting
    Signed-off-by: Val Knight <val@valknight.xyz>

--- classes/lock.py
+++ classes/lock.py
@@ -2,11 +2,12 @@ import time
 from server.util.code import generate_code
 maxTimeSinceLastRelock = 500
 
+
 class Lock():
     def __init__(self):
         self.key = None
         self.lastHeardFromClient = 0
-    
+
     def checkIfLockActive(self):
         if self.key is None:
             return False
@@ -14,11 +15,10 @@ class Lock():
         if not isActive:
             self.key = None
         return isActive
-    
-    
+
     def validateLock(self, key: str):
         return key == self.key
-    
+
     def generateLock(self):
         self.key = generate_code(n=128, onlyInts=False)
         return self.key
