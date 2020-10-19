c766b0e8e3a5189f2416ba28742e80f5b963da89
Val Knight <val@valknight.xyz>
2020-10-19 14:05:04 +0100

    Remember how to write classes in python 🤦‍♀️
    Signed-off-by: Val Knight <val@valknight.xyz>

--- classes/lock.py
+++ classes/lock.py
@@ -3,11 +3,11 @@ from server.util.code import generate_code
 maxTimeSinceLastRelock = 500
 
 class Lock():
-    def __init__():
+    def __init__(self):
         self.key = None
         self.lastHeardFromClient = 0
     
-    def checkIfLockActive():
+    def checkIfLockActive(self):
         if self.key is None:
             return False
         isActive = self.lastHeardFromClient - time.time() < maxTimeSinceLastRelock
@@ -16,9 +16,9 @@ class Lock():
         return isInactive
     
     
-    def validateLock(key: str):
+    def validateLock(self, key: str):
         return key == self.key
     
-    def generateLock():
+    def generateLock(self):
         self.key = generate_code(n=128, onlyInts=False)
-        return "a" //TODO: Make me randomly generated!
+        return self.key
